2d8e8b386f5c8ade29ca2c11ce6ae9c3c2ed1fc6
Antoine <antoine.demarly@gmail.com>
2013-08-09 00:09:37 +0200

    objets commande

--- app/models/Commande.java
+++ app/models/Commande.java
@@ -1,73 +1,49 @@
+package models;
 /***********************************************************************
  * Module:  Commande.java
- * Author:  antoi_000
+ * Author:  antoine
  * Purpose: Defines the Class Commande
  ***********************************************************************/
 
 import java.util.*;
 
-/** @pdOid 6f5ce6f3-0bdc-4a07-a8e6-e5d29188f0b1 */
 public class Commande {
-   /** @pdOid 4b19e3fe-a475-4dbf-8f8f-4fcac9c1fa43 */
-   public long comId;
-   /** @pdOid a36a59ac-560c-4d3d-aa73-596af566a60b */
-   public java.lang.String comReference;
-   /** @pdOid 7c842e20-5750-4797-a450-c8427c13bdce */
-   public java.lang.String comType;
-   
-   public java.util.Collection commandeAvoirStatut;
-   public java.util.Collection compositionCommande;
-   /** @pdRoleInfo migr=no name=Reception assc=recCom coll=java.util.Collection impl=java.util.HashSet mult=0..* side=A */
-   public java.util.Collection<Reception> reception;
-   
-   
-   /** @pdGenerated default getter */
-   public java.util.Collection<Reception> getReception() {
-      if (reception == null)
-         reception = new java.util.HashSet<Reception>();
-      return reception;
-   }
-   
-   /** @pdGenerated default iterator getter */
-   public java.util.Iterator getIteratorReception() {
-      if (reception == null)
-         reception = new java.util.HashSet<Reception>();
-      return reception.iterator();
-   }
-   
-   /** @pdGenerated default setter
-     * @param newReception */
-   public void setReception(java.util.Collection<Reception> newReception) {
-      removeAllReception();
-      for (java.util.Iterator iter = newReception.iterator(); iter.hasNext();)
-         addReception((Reception)iter.next());
-   }
-   
-   /** @pdGenerated default add
-     * @param newReception */
-   public void addReception(Reception newReception) {
-      if (newReception == null)
-         return;
-      if (this.reception == null)
-         this.reception = new java.util.HashSet<Reception>();
-      if (!this.reception.contains(newReception))
-         this.reception.add(newReception);
-   }
-   
-   /** @pdGenerated default remove
-     * @param oldReception */
-   public void removeReception(Reception oldReception) {
-      if (oldReception == null)
-         return;
-      if (this.reception != null)
-         if (this.reception.contains(oldReception))
-            this.reception.remove(oldReception);
-   }
-   
-   /** @pdGenerated default removeAll */
-   public void removeAllReception() {
-      if (reception != null)
-         reception.clear();
-   }
+   private long id;
+   private java.lang.String reference;
+   private java.lang.String type;
 
+    public Commande(long id, String reference, String type) {
+        this.id = id;
+        this.reference = reference;
+        this.type = type;
+    }
+
+    public long getid() {
+        return id;
+    }
+
+    public String getreference() {
+        return reference;
+    }
+
+    public void setreference(String reference) {
+        this.reference = reference;
+    }
+
+    public String gettype() {
+        return type;
+    }
+
+    public void settype(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Commande{" +
+                "id=" + id +
+                ", reference='" + reference + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
--- app/models/CompositionCommande.java
+++ app/models/CompositionCommande.java
@@ -1,17 +1,45 @@
+package models;
 /***********************************************************************
  * Module:  CompositionCommande.java
- * Author:  antoi_000
+ * Author:  antoine
  * Purpose: Defines the Class CompositionCommande
  ***********************************************************************/
 
 import java.util.*;
 
-/** @pdOid b2e9b623-4009-4fd3-a334-f1b6a12770ed */
 public class CompositionCommande {
-   /** @pdOid 564fe236-4884-48ff-abac-ee807eacc117 */
-   public float comComCliQuantite;
-   
-   public Produit produitA;
-   public Commande commandeB;
+   private float quantite;
+   private long idProduit;
+   private long idCommande;
 
+    public CompositionCommande(float quantite, long idProduit, long idCommande) {
+        this.quantite = quantite;
+        this.idProduit = idProduit;
+        this.idCommande = idCommande;
+    }
+
+    public float getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(float quantite) {
+        this.quantite = quantite;
+    }
+
+    public long getIdProduit() {
+        return idProduit;
+    }
+
+    public long getIdCommande() {
+        return idCommande;
+    }
+
+    @Override
+    public String toString() {
+        return "CompositionCommande{" +
+                "quantite=" + quantite +
+                ", idProduit=" + idProduit +
+                ", idCommande=" + idCommande +
+                '}';
+    }
 }
