ab59f1f6ea04940d994dad6f0e118dcfc986fdbe
Antoine <antoine.demarly@gmail.com>
2013-08-10 15:40:02 +0200

    generation get/set

--- app/models/Commande.java
+++ app/models/Commande.java
@@ -32,4 +32,123 @@ public class Commande extends Model{
    private List<StatutCommande> statutCommandes;
 
 
+    /**
+     * Gets reference.
+     *
+     * @return Value of reference.
+     */
+    public String getReference() {
+        return reference;
+    }
+
+    /**
+     * Gets statutCommandes.
+     *
+     * @return Value of statutCommandes.
+     */
+    public List<StatutCommande> getStatutCommandes() {
+        return statutCommandes;
+    }
+
+    /**
+     * Gets type.
+     *
+     * @return Value of type.
+     */
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     * Gets receptions.
+     *
+     * @return Value of receptions.
+     */
+    public List<Reception> getReceptions() {
+        return receptions;
+    }
+
+    /**
+     * Sets new reference.
+     *
+     * @param reference New value of reference.
+     */
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    /**
+     * Gets partenaire.
+     *
+     * @return Value of partenaire.
+     */
+    public Partenaire getPartenaire() {
+        return partenaire;
+    }
+
+    /**
+     * Sets new statutCommandes.
+     *
+     * @param statutCommandes New value of statutCommandes.
+     */
+    public void setStatutCommandes(List<StatutCommande> statutCommandes) {
+        this.statutCommandes = statutCommandes;
+    }
+
+    /**
+     * Sets new compositionCommandes.
+     *
+     * @param compositionCommandes New value of compositionCommandes.
+     */
+    public void setCompositionCommandes(List<CompositionCommande> compositionCommandes) {
+        this.compositionCommandes = compositionCommandes;
+    }
+
+    /**
+     * Sets new type.
+     *
+     * @param type New value of type.
+     */
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    /**
+     * Sets new receptions.
+     *
+     * @param receptions New value of receptions.
+     */
+    public void setReceptions(List<Reception> receptions) {
+        this.receptions = receptions;
+    }
+
+    /**
+     * Sets new partenaire.
+     *
+     * @param partenaire New value of partenaire.
+     */
+    public void setPartenaire(Partenaire partenaire) {
+        this.partenaire = partenaire;
+    }
+
+    /**
+     * Gets compositionCommandes.
+     *
+     * @return Value of compositionCommandes.
+     */
+    public List<CompositionCommande> getCompositionCommandes() {
+        return compositionCommandes;
+    }
+
+    @Override
+    public String toString() {
+        return "Commande{" +
+                "reference='" + reference + '\'' +
+                ", type=" + type +
+                ", receptions=" + receptions +
+                ", compositionCommandes=" + compositionCommandes +
+                ", partenaire=" + partenaire +
+                ", statutCommandes=" + statutCommandes +
+                '}';
+    }
 }
--- app/models/Partenaire.java
+++ app/models/Partenaire.java
@@ -35,4 +35,181 @@ public class Partenaire extends Model{
     @OneToMany(mappedBy = "partenaire")
     private List<Contact> contacts;
 
+
+    /**
+     * Gets coordonneePostales.
+     *
+     * @return Value of coordonneePostales.
+     */
+    public List<CoordonneePostale> getCoordonneePostales() {
+        return coordonneePostales;
+    }
+
+    /**
+     * Gets type.
+     *
+     * @return Value of type.
+     */
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     * Gets reference.
+     *
+     * @return Value of reference.
+     */
+    public String getReference() {
+        return reference;
+    }
+
+    /**
+     * Sets new capital.
+     *
+     * @param capital New value of capital.
+     */
+    public void setCapital(int capital) {
+        this.capital = capital;
+    }
+
+    /**
+     * Gets actif.
+     *
+     * @return Value of actif.
+     */
+    public boolean isActif() {
+        return actif;
+    }
+
+    /**
+     * Gets nom.
+     *
+     * @return Value of nom.
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Sets new nom.
+     *
+     * @param nom New value of nom.
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * Sets new commandes.
+     *
+     * @param commandes New value of commandes.
+     */
+    public void setCommandes(List<Commande> commandes) {
+        this.commandes = commandes;
+    }
+
+    /**
+     * Sets new reference.
+     *
+     * @param reference New value of reference.
+     */
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    /**
+     * Gets capital.
+     *
+     * @return Value of capital.
+     */
+    public int getCapital() {
+        return capital;
+    }
+
+    /**
+     * Sets new type.
+     *
+     * @param type New value of type.
+     */
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    /**
+     * Sets new coordonneePostales.
+     *
+     * @param coordonneePostales New value of coordonneePostales.
+     */
+    public void setCoordonneePostales(List<CoordonneePostale> coordonneePostales) {
+        this.coordonneePostales = coordonneePostales;
+    }
+
+    /**
+     * Gets contacts.
+     *
+     * @return Value of contacts.
+     */
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    /**
+     * Gets commandes.
+     *
+     * @return Value of commandes.
+     */
+    public List<Commande> getCommandes() {
+        return commandes;
+    }
+
+    /**
+     * Sets new contacts.
+     *
+     * @param contacts New value of contacts.
+     */
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    /**
+     * Gets noSIRET.
+     *
+     * @return Value of noSIRET.
+     */
+    public String getNoSIRET() {
+        return noSIRET;
+    }
+
+    /**
+     * Sets new noSIRET.
+     *
+     * @param noSIRET New value of noSIRET.
+     */
+    public void setNoSIRET(String noSIRET) {
+        this.noSIRET = noSIRET;
+    }
+
+    /**
+     * Sets new actif.
+     *
+     * @param actif New value of actif.
+     */
+    public void setActif(boolean actif) {
+        this.actif = actif;
+    }
+
+    @Override
+    public String toString() {
+        return "Partenaire{" +
+                "nom='" + nom + '\'' +
+                ", noSIRET='" + noSIRET + '\'' +
+                ", capital=" + capital +
+                ", actif=" + actif +
+                ", reference='" + reference + '\'' +
+                ", type=" + type +
+                ", commandes=" + commandes +
+                ", coordonneePostales=" + coordonneePostales +
+                ", contacts=" + contacts +
+                '}';
+    }
 }
