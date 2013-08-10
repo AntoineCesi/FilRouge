95ea432d620be0faee22c68aa7d5b21807f8dc0e
Jérémie Lapert <jeremie.lapert@achatpublic.com>
2013-08-10 15:38:30 +0200

    Début envois de mail

---
+++ app/controllers/Mailer/Mails.java
@@ -0,0 +1,24 @@
+package controllers.Mailer;
+
+import play.mvc.Mailer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jeremielapert
+ * Date: 09/08/13
+ * Time: 18:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class Mails extends Mailer{
+
+    public static void alert() {
+//        setSubject("Welcome %s", user.name);
+//        addRecipient(user.email);
+//        setFrom("Me <me@me.com>");
+//        EmailAttachment attachment = new EmailAttachment();
+//        attachment.setDescription("A pdf document");
+//        attachment.setPath(Play.getFile("rules.pdf").getPath());
+//        addAttachment(attachment);
+//        send(user);
+    }
+}
