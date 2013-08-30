16bba738100d9f97b5472546aedcddccf7fe603c
Jérémie Lapert <jeremie.lapert@achatpublic.com>
2013-08-30 12:06:10 +0200

    Ajout des templates pour les mails et du code associé dans le contrôler

--- app/controllers/Mailer/Mails.java
+++ app/controllers/Mailer/Mails.java
@@ -1,5 +1,7 @@
 package controllers.Mailer;
 
+import models.Alerte;
+import models.Salarie;
 import play.mvc.Mailer;
 
 /**
@@ -7,14 +9,22 @@ import play.mvc.Mailer;
  */
 public class Mails extends Mailer{
 
-    public static void alert() {
-//        setSubject("Welcome %s", user.name);
-//        addRecipient(user.email);
-//        setFrom("Me <me@me.com>");
+    public static void alert(Salarie salarie, Alerte alerte) {
+        setSubject("Welcome %s", salarie.nom + " " + salarie.prenom);
+        addRecipient(salarie.courriel);
+        setFrom("Robot <robot@thecompany.com>");
 //        EmailAttachment attachment = new EmailAttachment();
 //        attachment.setDescription("A pdf document");
 //        attachment.setPath(Play.getFile("rules.pdf").getPath());
 //        addAttachment(attachment);
-//        send(user);
+        send(salarie, alerte);
     }
+
+   public static void lostPassword(Salarie salarie) {
+       String newpassword = salarie.mdp;
+       setFrom("Robot <robot@thecompany.com>");
+       setSubject("Your password has been reset");
+       addRecipient(salarie.courriel);
+       send(salarie, newpassword);
+   }
 }
