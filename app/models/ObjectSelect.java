36e88444545e6679d578e920158c866f3d72c328
Antoine <antoine.demarly@gmail.com>
2013-09-03 21:52:22 +0200

    CRUD Lot et Reception

---
+++ app/models/ObjectSelect.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: antoi_000
+ * Date: 03/09/13
+ * Time: 21:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class ObjectSelect {
+}
